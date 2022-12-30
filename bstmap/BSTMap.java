593b5c20480c9fa6b6eefb4f1888f2caa8a66530
Zhang9340 <m17610066172@163.com>
2022-12-30 15:27:04 -0500

    first commit

---
+++ bstmap/BSTMap.java
@@ -0,0 +1,104 @@
+package bstmap;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K extends Comparable<K>,V> implements Map61B<K,V>{
+    private int size;
+    private EntryNode root;
+    public BSTMap(){
+
+    }
+
+
+    private class EntryNode{
+        private K key;
+        private V value;
+        EntryNode left;
+        EntryNode right;
+
+        EntryNode(K key,V value){
+            this.key=key;
+            this.value= value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+
+    }
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+
+
+
+    @Override
+    public void put(K key, V value) {
+        if(key== null || value==null) throw new IllegalArgumentException("Input key or value is null");
+        if (root==null){
+            root= new EntryNode(key,value);
+
+        }else {
+            put(root, key,value);
+
+        }
+
+    }
+    private EntryNode put(EntryNode node , K key, V value){
+          if(node == null) return new EntryNode(key, value);
+          int cmd = key.compareTo(node.key);
+          if(cmd<0) node.left= put(node.left, key, value);
+          else if (cmd>0)   node .right=put(node.right, key, value);
+          else node.value= value;
+
+          return node;
+
+    }
+
+    @Override
+    public Set keySet() {
+        return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        return null;
+    }
+
+
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+}
